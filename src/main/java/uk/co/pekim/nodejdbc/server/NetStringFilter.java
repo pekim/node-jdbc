1ddb848480a539fb20ff330b35e673aacac0d5df
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-04-08 23:10:12 +0100

    Additional comments.

--- src/main/java/uk/co/pekim/nodejdbc/server/NetStringFilter.java
+++ src/main/java/uk/co/pekim/nodejdbc/server/NetStringFilter.java
@@ -34,6 +34,7 @@ public class NetStringFilter extends BaseFilter {
         final Buffer sourceBuffer = context.getMessage();
         final int sourceBufferLength = sourceBuffer.remaining();
 
+        // Find end of length digits.
         boolean foundLengthDelimiter = false;
         while (sourceBuffer.position() < sourceBufferLength) {
             if (sourceBuffer.get() == LENGTH_DELIMITER) {
@@ -42,53 +43,60 @@ public class NetStringFilter extends BaseFilter {
             }
         }
 
+        // Not found.
         if (!foundLengthDelimiter) {
             sourceBuffer.rewind();
             sourceBuffer.position(0);
             return context.getStopAction(sourceBuffer);
         }
 
+        // Get string length.
         int lengthLength = sourceBuffer.position() - 1;
         final int stringLength = getStringLength(sourceBuffer, lengthLength);
         final int requiredBufferLength = lengthLength + 1 + stringLength + 1;
 
+        // Not enough buffer to contain the full string.
         if (sourceBufferLength < requiredBufferLength) {
             sourceBuffer.rewind();
             sourceBuffer.position(0);
             return context.getStopAction(sourceBuffer);
         }
 
+        // Create a buffer with the remaining byte (following this string), if any.
         Buffer remainder = null;
         if (sourceBufferLength > requiredBufferLength) {
             remainder = sourceBuffer.split(requiredBufferLength);
         }
 
-        // Read body string.
+        // Read the string payload.
         final byte[] stringBytes = new byte[stringLength];
         sourceBuffer.get(stringBytes);
         final String string = new String(stringBytes, CHARSET);
         
+        // Consume the delimiting comma.
         byte stringDelimiter = sourceBuffer.get();
         if (stringDelimiter != STRING_DELIMITER) {
             LOGGER.warn("Expected '" + STRING_DELIMITER + "' delimiting netstring but found '" + ((char) stringDelimiter) + "'");
         }
 
         LOGGER.info("JSON received : " + string);
-        
+
+        // The extracted string is the for the next filter.
         context.setMessage(string);
         
         sourceBuffer.tryDispose();
 
-        System.out.println("'" + remainder.toStringContent() + "'");
         return context.getInvokeAction(remainder);
     }
     
     @Override
     public NextAction handleWrite(FilterChainContext context) throws IOException {
+        // Create the netstring.
         final String stringToWrap = context.getMessage();
         final String wrappedString = "" + stringToWrap.length() + LENGTH_DELIMITER + stringToWrap + STRING_DELIMITER;
         LOGGER.info("write : " + stringToWrap);
 
+        // Put the netstirng in a buffer.
         @SuppressWarnings("unchecked")
         MemoryManager<Buffer> memoryManager = context.getConnection().getTransport().getMemoryManager();
         Buffer buffer = Buffers.wrap(memoryManager, wrappedString);
@@ -97,8 +105,6 @@ public class NetStringFilter extends BaseFilter {
 
         return context.getInvokeAction();
     }
-
-
 
     private int getStringLength(final Buffer buffer, final int byteCount) {
         final int oldPosition = buffer.position();

de71522009ca6e6500ef442c7b0350493f87668c
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-04-08 23:03:47 +0100

    Process chunked input correctly.

--- src/main/java/uk/co/pekim/nodejdbc/server/NetStringFilter.java
+++ src/main/java/uk/co/pekim/nodejdbc/server/NetStringFilter.java
@@ -4,6 +4,7 @@
 package uk.co.pekim.nodejdbc.server;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import org.glassfish.grizzly.Buffer;
 import org.glassfish.grizzly.filterchain.BaseFilter;
@@ -23,6 +24,8 @@ import org.slf4j.LoggerFactory;
 public class NetStringFilter extends BaseFilter {
     private static final Logger LOGGER = LoggerFactory.getLogger(NetStringFilter.class);
 
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+
     private static final char STRING_DELIMITER = ',';
     private static final char LENGTH_DELIMITER = ':';
 
@@ -40,6 +43,8 @@ public class NetStringFilter extends BaseFilter {
         }
 
         if (!foundLengthDelimiter) {
+            sourceBuffer.rewind();
+            sourceBuffer.position(0);
             return context.getStopAction(sourceBuffer);
         }
 
@@ -48,6 +53,8 @@ public class NetStringFilter extends BaseFilter {
         final int requiredBufferLength = lengthLength + 1 + stringLength + 1;
 
         if (sourceBufferLength < requiredBufferLength) {
+            sourceBuffer.rewind();
+            sourceBuffer.position(0);
             return context.getStopAction(sourceBuffer);
         }
 
@@ -59,7 +66,7 @@ public class NetStringFilter extends BaseFilter {
         // Read body string.
         final byte[] stringBytes = new byte[stringLength];
         sourceBuffer.get(stringBytes);
-        final String string = new String(stringBytes);
+        final String string = new String(stringBytes, CHARSET);
         
         byte stringDelimiter = sourceBuffer.get();
         if (stringDelimiter != STRING_DELIMITER) {
@@ -70,19 +77,18 @@ public class NetStringFilter extends BaseFilter {
         
         context.setMessage(string);
         
-        // We can try to dispose the buffer
         sourceBuffer.tryDispose();
-        
+
+        System.out.println("'" + remainder.toStringContent() + "'");
         return context.getInvokeAction(remainder);
     }
     
     @Override
     public NextAction handleWrite(FilterChainContext context) throws IOException {
         final String stringToWrap = context.getMessage();
+        final String wrappedString = "" + stringToWrap.length() + LENGTH_DELIMITER + stringToWrap + STRING_DELIMITER;
         LOGGER.info("write : " + stringToWrap);
 
-        String wrappedString = "" + stringToWrap.length() + LENGTH_DELIMITER + stringToWrap + STRING_DELIMITER;
-        
         @SuppressWarnings("unchecked")
         MemoryManager<Buffer> memoryManager = context.getConnection().getTransport().getMemoryManager();
         Buffer buffer = Buffers.wrap(memoryManager, wrappedString);
@@ -103,6 +109,6 @@ public class NetStringFilter extends BaseFilter {
         buffer.get(bytes);
         
         buffer.position(oldPosition);
-        return Integer.parseInt(new String(bytes));
+        return Integer.parseInt(new String(bytes, CHARSET));
     }
 }
